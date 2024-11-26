286f7cbaf9c0a71f0cf6e801494496dbe53410c1
zineb511 <zinebskalli511@gmail.com>
2024-11-26 12:14:38 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true
-    },
-    basePath: '/zinePortfolio',
-    assetPrefix: '/zinePortfolio'
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {};
+
+export default nextConfig;
