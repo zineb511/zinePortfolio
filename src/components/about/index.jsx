0631ef71358581bd2d5dc49c20c194954fdf869e
zineb511 <zinebskalli511@gmail.com>
2024-11-26 12:36:02 +0100

    Description de vos modifications

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -11,14 +11,14 @@ const AboutDetails = () => {
           }
         >
           <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
-            Zineb EL GHILANI
+            mmanal EL GHILANI
           </h2>
           <p className="font-light  text-xs sm:text-sm md:text-base   ">
-          Passionnée par l'univers du design et de la création visuelle, je suis une Motion Designer et Infographiste expérimentée spécialisée dans la conception de contenus dynamiques et innovants. Avec une maîtrise approfondie des outils phares tels qu'Adobe After Effects, Premiere Pro, Illustrator, Photoshop, InDesign et XD, je donne vie à vos idées à travers des animations captivantes, des designs élégants et des expériences visuelles mémorables.
+          Passionnée par l&apos;univers du design et de la création visuelle, je suis une Motion Designer et Infographiste expérimentée spécialisée dans la conception de contenus dynamiques et innovants. Avec une maîtrise approfondie des outils phares tels qu&apos;Adobe After Effects, Premiere Pro, Illustrator, Photoshop, InDesign et XD, je donne vie à vos idées à travers des animations captivantes, des designs élégants et des expériences visuelles mémorables.
 
-Mon expertise s'étend du motion design pour des vidéos promotionnelles, publicités, ou vidéos explicatives, à la création graphique pour des supports print et digitaux : logos, affiches, brochures, interfaces utilisateur, et plus encore. Mon objectif est de combiner créativité et technicité pour offrir des solutions uniques qui captivent et inspirent.
+Mon expertise s&apos;étend du motion design pour des vidéos promotionnelles, publicités, ou vidéos explicatives, à la création graphique pour des supports print et digitaux : logos, affiches, brochures, interfaces utilisateur, et plus encore. Mon objectif est de combiner créativité et technicité pour offrir des solutions uniques qui captivent et inspirent.
 
-Que ce soit pour raconter une histoire en mouvement ou concevoir une identité visuelle percutante, je m'engage à transformer vos projets en œuvres d'art visuelles qui reflètent vos valeurs et vos ambitions.
+Que ce soit pour raconter une histoire en mouvement ou concevoir une identité visuelle percutante, je m&apos;engage à transformer vos projets en œuvres d&apos;art visuelles qui reflètent vos valeurs et vos ambitions.
           </p>
         </ItemLayout>
 
